d9ac67e4d197de33f860abb0819a0e79f30c47e2
Irrfan <kaungkhant12359@gmail.com>
2026-05-01 00:39:42 +0630

    Cleanup CORS headers

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,15 +1,5 @@
 <?php
 
-header('Access-Control-Allow-Origin: https://red-ocean-00f95d100.7.azurestaticapps.net');
-header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE');
-header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
-header('Access-Control-Allow-Credentials: true');
-
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    http_response_code(200);
-    exit;
-}
-
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
